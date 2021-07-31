7f40a056788b0b44dfaf2e3d96845154c8b662eb
Harrison Cramer <harrisoncramer@gmail.com>
2021-07-31 14:34:02 -0400

    Added setup file.

---
+++ server/db/setup.sql
@@ -0,0 +1,29 @@
+/*
+These are the tables required to run this application. 
+Please create a separate database for these tables inside of your D inside of your DB, e.g. CREATE DATABASE wobble_chat; 
+Then 
+ */
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  username varchar(50) UNIQUE,
+  password varchar(50),
+  isLoggedIn boolean DEFAULT true
+);
+
+CREATE TABLE IF NOT EXISTS questions (
+  id SERIAL PRIMARY KEY,
+  isAnswered boolean DEFAULT false NOT NULL,
+  creator integer REFERENCES users(id),
+  url varchar(100) UNIQUE NOT NULL,
+  title varchar(500) NOT NULL,
+  description text NOT NULL,
+  /* Can you do a foreign key reference of a boolean in another table? The boolean is not a key... */
+  isOpen boolean DEFAULT false NOT NULL
+);
+CREATE TABLE IF NOT EXISTS messages (
+  id SERIAL PRIMARY KEY,
+  dateCreated date NOT NULL,
+  questionId integer REFERENCES questions(id),
+  content text NOT NULL
+);
