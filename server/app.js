a35dca08d668c2d864090c3dc3770f0dbb19973a
Harrison Cramer <harrisoncramer@gmail.com>
2021-07-31 14:37:35 -0400

    added debug router, structure, and initial db setup

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const debugRouter = require("./routers/debug");
+
+if (process.env.NODE_ENV === "development") app.use(debugRouter);
 
 app.get("/api/hello", (req, res) => {
   res.status(200).json({ hello: "world" });
