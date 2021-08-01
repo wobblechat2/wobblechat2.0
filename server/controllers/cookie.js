1674c517f503c3c9cb7a1710d1be090bd8cc2bab
Harrison Cramer <harrisoncramer@gmail.com>
2021-07-31 23:47:18 -0400

    Added sign up and sign in routes, and setting SSID cookie for users.

---
+++ server/controllers/cookie.js
@@ -0,0 +1,22 @@
+const cookieController = {};
+
+// Set cookie on user (of their ID) upon successful sign up or sign in.
+cookieController.setCookie = (_req, res, next) => {
+  try {
+    const { id } = res.locals;
+    res.cookie("ssid", id);
+    return next();
+  } catch (err) {
+    return next({
+      status: 500,
+      message: err.message,
+    });
+  }
+};
+
+// setSSIDCookie - store the user id in a cookie
+cookieController.setSSIDCookie = (req, res, next) => {
+  // write code here
+};
+
+module.exports = cookieController;
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,10 +1,25 @@
 const { Router } = require("express");
 const userController = require("../controllers/user");
+const cookieController = require("../controllers/cookie");
 
 const userRouter = Router();
 
-userRouter.post("/signup", userController.createUser, (req, res, next) => {
-  res.status(200).send({ id: res.locals.id });
-});
+userRouter.post(
+  "/signup",
+  userController.createUser,
+  cookieController.setCookie,
+  (_req, res) => {
+    res.status(200).send({ id: res.locals.id });
+  }
+);
+
+userRouter.post(
+  "/signin",
+  userController.signInUser,
+  cookieController.setCookie,
+  (_req, res) => {
+    res.status(200).send({ id: res.locals.id });
+  }
+);
 
 module.exports = userRouter;
