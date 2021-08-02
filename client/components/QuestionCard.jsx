ea7b5289271d97a2078d1fbe268784daaeed9f33
Andrew Kessinger <andrew.kessinger@gmail.com>
2021-08-02 16:22:02 +0200

    Updated front end components: main app view

--- client/components/QuestionCard.jsx
+++ client/components/QuestionCard.jsx
@@ -3,18 +3,44 @@ import React from 'react';
 // import { faQuestionCircle } from '@fortawesome/free-regular-svg-icons';
 import { Link } from 'react-router-dom';
 
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
 const QuestionCard = (props) => {
   //props from QuestionsContainer get assigned to each instance of QuestionCard
-  const { questionId, isActive, title, description, chatURL } = props;
+  const { key, isActive, title, description, chatURL } = props;
+
+
+// let answerButton = '';
+
+//   if (isActive===true){
+//    answerButton = '<Button variant="primary">Answer this question</Button>';
+//   }
+
+
 
   return (
-    <div>
-      QuestionID: {questionId} <br />
-      IsActive: {isActive} <br />
-      Title: {title} <br />
-      Description: {description} <br />
-      ChatURL: {chatURL} <br />
-    </div>
+    <>
+
+
+
+
+<Card key={key}>
+  <Card.Body>
+
+<div className="question-container">
+  <Card.Title>{title}</Card.Title>
+  <Button variant="primary">Answer question</Button>
+</div>
+    <Card.Text>
+    {description}
+    </Card.Text>
+  </Card.Body>
+</Card>
+
+</>
+
   );
 };
 
