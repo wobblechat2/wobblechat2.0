24da71ea3ab2d26215adf864341359d6ab59057b
Konstantin <konst@ntinh.am>
2021-08-03 19:40:00 -0400

    added new database

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
+const our_username = process.env.DBUSERNAME;
+const our_password = process.env.DBPASSWORD;
 
 // Connect to our database and assign the result of that connection to a new object
 // Then export that object so that we can make queries to our DB.
 // This object is used inside of our controllers.
 const uri =
-  "postgres://pyrlesph:y49KNIFGACz5VEWNtTL8QH8auBdno8ha@chunee.db.elephantsql.com/pyrlesph";
+  `postgres://${our_username}:${our_password}@36ed0620020c572f2d626c93b36180bc.postgres.database.azure.com/wobblechatdb?sslmode=require`;
 const pool = new Pool({
   connectionString: uri,
 }
