f54478ad9575456f1f912cf20bafb66b39891a34
lolfill <lolfill0104@gmail.com>
2021-08-03 20:33:20 -0400

    test

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Signup from "./components/Signup";
-import Login from "./Login";
-import ProtectedRoute from "./ProtectedRoute";
+import Login from "./components/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
 import MainAppContainer from "./containers/MainAppContainer";
 import "./stylesheets/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
