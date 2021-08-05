27debb81d5ccc625711534e06c3a54e3a3fbd32c
lolfill <lolfill0104@gmail.com>
2021-08-05 18:17:45 -0400

    styling/logout fix

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader'
+      },
     ],
   },
   plugins: [
